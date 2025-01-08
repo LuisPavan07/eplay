d0b18d303d5a911416edf870b2926cbfa85d79c3
LuisPavan07 <luihoracio@gmail.com>
2025-01-08 14:57:15 -0300

    adicionando funcionalidade ao carrinho de compras e navegação com ancoras para as categorias

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,60 @@
+import ProductsList from '../../components/ProductsList'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+import { Game } from '../Home'
+import { useEffect, useState } from 'react'
+
+import {
+  useGetActionGamesQuery,
+  useGetSportGamesQuery,
+  useGetSimulationGamesQuery,
+  useGetFightGamesQuery,
+  useGetRpgGamesQuery
+} from '../../services/api'
+
+const Categories = () => {
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: sportGames } = useGetSportGamesQuery()
+  const { data: simulationGames } = useGetSimulationGamesQuery()
+  const { data: fightGames } = useGetFightGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
+
+  if (actionGames && sportGames && simulationGames && fightGames && rpgGames) {
+    return (
+      <>
+        <ProductsList
+          games={actionGames}
+          title="Ação"
+          background="black"
+          id="action"
+        />
+        <ProductsList
+          games={sportGames}
+          title="Esportes"
+          background="gray"
+          id="sports"
+        />
+        <ProductsList
+          games={fightGames}
+          title="Luta"
+          background="black"
+          id="fight"
+        />
+        <ProductsList games={rpgGames} title="RPG" background="gray" id="rpg" />
+        <ProductsList
+          games={simulationGames}
+          title="Simulação"
+          background="black"
+          id="simulation"
+        />
+      </>
+    )
+  }
+
+  return <h4>Carregando</h4>
+}
+
+export default Categories
